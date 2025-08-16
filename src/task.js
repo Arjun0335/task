0deee6e21fb86b833c4fe6f6ed95206e357c005a
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-08-16 12:11:13 +0530

    Create task.js

---
+++ src/task.js
@@ -0,0 +1,10 @@
+const tasks = {
+  "2025-08-18": ["Finish math assignment", "Read chapter 4 of AI book"],
+  "2025-08-19": ["Prepare for viva", "Submit project report"],
+  "2025-08-20": ["Gym session", "Work on IoT project"],
+  "2025-08-21": ["Team meeting at 5 PM", "Study for ML exam"],
+  "2025-08-22": ["Complete React website", "Revise DSA topics"],
+  // add more dates up to 1 month
+};
+
+export default tasks;
